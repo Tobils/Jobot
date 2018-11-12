5eb2b3558e14961fc6c6b7e264610421ff9a0e4c
Ade Suhada <ade.suhada@mail.ugm.ac.id>
2018-11-12 22:49:19 +0700

    commit ke 2 implementasi webhook.php dan tebakkode_m.php

--- application/models/Tebakkode_m.php
+++ application/models/Tebakkode_m.php
@@ -13,26 +13,25 @@ class Tebakkode_m extends CI_Model {
     $this->db->set('signature', $signature)
     ->set('events', $body)
     ->insert('eventlog');
-
     return $this->db->insert_id();
   }
 
-  // Users
-  function getUser($userId)
-  {
-    $data = $this->db->where('user_id', $userId)->get('users')->row_array();
-    if(count($data) > 0) return $data;
-    return false;
-  }
+ /// Users
+ function getUser($userId)
+ {
+   $data = $this->db->where('user_id', $userId)->get('users')->row_array();
+   if(count($data) > 0) return $data;
+   return false;
+ }
 
-  function saveUser($profile)
-  {
-    $this->db->set('user_id', $profile['userId'])
-      ->set('display_name', $profile['displayName'])
-      ->insert('users');
+ function saveUser($profile)
+ {
+   $this->db->set('user_id', $profile['userId'])
+     ->set('display_name', $profile['displayName'])
+     ->insert('users');
 
-    return $this->db->insert_id();
-  }
+   return $this->db->insert_id();
+ }
 
   // Question
   function getQuestion($questionNum)
@@ -40,37 +39,37 @@ class Tebakkode_m extends CI_Model {
     $data = $this->db->where('number', $questionNum)
       ->get('questions')
       ->row_array();
- 
+
     if(count($data)>0) return $data;
     return false;
   }
- 
+
   function isAnswerEqual($number, $answer)
   {
     $this->db->where('number', $number)
       ->where('answer', $answer);
- 
+
     if(count($this->db->get('questions')->row()) > 0)
       return true;
- 
+
     return false;
   }
- 
+
   function setUserProgress($user_id, $newNumber)
   {
     $this->db->set('number', $newNumber)
       ->where('user_id', $user_id)
       ->update('users');
- 
+
     return $this->db->affected_rows();
   }
- 
+
   function setScore($user_id, $score)
   {
     $this->db->set('score', $score)
       ->where('user_id', $user_id)
       ->update('users');
- 
+
     return $this->db->affected_rows();
   }
 
