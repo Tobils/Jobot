e9acd7abb4fa0ab34e988c003afdfcce1c4b86fa
Ade Suhada <ade.suhada@mail.ugm.ac.id>
2018-11-15 14:51:22 +0700

    edit aja

---
+++ application/controllers/Test.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+	public function index()
+	{
+		$flexTemplate = file_get_contents(APPPATH.'../flex_message.json'); // load template flex message
+
+        print_r($flexTemplate);
+                            $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
+                                'replyToken' => $event['replyToken'],
+                                'messages'   => [
+                                    [
+                                        'type'     => 'flex',
+                                        'altText'  => 'Semangat menggapai mimpi !',
+                                        'contents' => json_decode($flexTemplate)
+                                    ]
+                                ],
+                            ]);
+                            print_r($result);
+
+	}
+}
