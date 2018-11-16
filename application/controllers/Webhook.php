606edf23f88d1fecf0309434be4ff0afe514beef
Ade Suhada <ade.suhada@mail.ugm.ac.id>
2018-11-16 14:41:38 +0700

    edit webhook.php

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -120,9 +120,9 @@ class Webhook extends CI_Controller {
        
     }
     elseif(strtolower($userMessage) == 'flex'){
+      $flexTemplate = file_get_contents(APPPATH.'/controllers/flex_message.json'); // load template flex message
       $stickerMessageBuilder = new StickerMessageBuilder(1, 100);
       $this->bot->replyMessage($event['replyToken'], $stickerMessageBuilder);
-      $flexTemplate = file_get_contents(APPPATH.'/controllers/flex_message.json'); // load template flex message
       $this->bot->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
           'replyToken' => $event['replyToken'],
           'messages'   => [
