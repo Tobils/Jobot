a737b8b318cf3b33f99df38ac1ae920b174927bf
Ade Suhada <ade.suhada@mail.ugm.ac.id>
2018-11-19 11:16:38 +0700

    added to flex message

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -6,7 +6,7 @@ class Test extends CI_Controller {
 	{
 		$flexTemplate = file_get_contents(APPPATH.'/controllers/flex_message.json'); // load template flex message
 
-        print_r($flexTemplate);
+        //print_r($flexTemplate);
         $js_dcd = json_decode($flexTemplate);
         print_r($js_dcd);
                             $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
